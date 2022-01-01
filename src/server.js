56b9e62af3197b5731051c0587f6971f96ca880c
okok0415 <dlawjdals11@naver.com>
2022-01-01 22:35:05 +0900

    220101

--- src/server.js
+++ src/server.js
@@ -16,39 +16,21 @@ const server = http.createServer(app);
 const io = socketIO(server);
 
 io.on("connection", socket => {
+    socket.onAny((event) => {
+        console.log(`socket Event: ${event}`);
+    })
     socket.on("room", (roomName, done) => {
-        console.log(roomName);
-        setTimeout(() => {
-            done("hello from the backend");
-        }, 5000)
+        socket.join(roomName);
+        done();
+        socket.to(roomName).emit("welcome");
     });
+    socket.on("disconnecting", () => {
+        socket.rooms.forEach((room) => socket.to(room).emit("bye"));
+    })
+    socket.on("new_message", (msg, room, done) => {
+        socket.to(room).emit("new_message", msg);
+        done();
+    })
 })
-/* 
-
-const wss = new WebSocket.Server({ server });
-
-function onSocketClose() {
-    console.log("Disconnected from the Browser ❌");
-}
-
-const sockets = [];
-
-wss.on("connection", (socket) => {
-    sockets.push(socket);
-    socket["nickname"] = "Anon";
-    console.log("Connected to Browser ✅");
-    socket.on("close", onSocketClose);
-    socket.on("message", (msg) => {
-        const message = JSON.parse(msg);
-        switch (message.type) {
-            case "new_message":
-                sockets.forEach((aSocket) =>
-                    aSocket.send(`${socket.nickname}: ${message.payload}`)
-                );
-            case "nickname":
-                socket["nickname"] = message.payload;
-        }
-    });
-}); */
 
 server.listen(3000, handleListen);
